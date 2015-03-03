58f31b3a831050bb19cb1485e2e737d5707464e2
Marco Biedermann <marco.biedermann@gmx.de>
2015-03-03 18:56:51 +0100

    remove jQuery scroll function

--- source/js/script.js
+++ source/js/script.js
@@ -4,30 +4,4 @@
 
   document.createElement('picture');
 
-  // scrollTo
-
-  $('.scroll-to').on('click', function(e) {
-
-    e.preventDefault();
-
-    $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-    }, 800);
-
-    return false;
-
-  });
-
-  // scrollTop
-
-  $('.scroll-top').on('click', function(e) {
-
-    e.preventDefault();
-
-    $('html, body').animate({ scrollTop: 0 }, 'slow');
-
-    return false;
-
-  });
-
 })();
